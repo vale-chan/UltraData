8a95f6914f17f027fd0482e4591c371bbb0ad758
vale-chan <valeria_luce@hotmail.com>
2022-02-12 17:15:59 +0100

    added option for choosing saving folder

--- src/ultra2nice.py
+++ src/ultra2nice.py
@@ -1,6 +1,8 @@
+from unittest import skip
 import pandas as pd
 import numpy as np
 import argparse
+import os
 import yaml
 
 
@@ -14,18 +16,20 @@ def main():
                           help="Path to the MasterCodebook to be used by niceplots.")
     cli_args.add_argument('--pathtoconfigfile', type=str, action='store', required=True,
                           help="Path to the config file that overrides the defaults of the nice-plot config file")
+    cli_args.add_argument('--saveto', type=str, action='store', required=False, default="",
+                          help='Path to where the plots shoulde be saved')
     cli_args.add_argument('--plottype', type=str, action='store', required=True,
                           help="Choose the desired plot type (bars, lines, histograms")
     cli_args.add_argument('--outputname', type=str, action='store', required=True,
                           help='Choose the name for your plots')
     cli_args.add_argument('--saveas', type=str, action='store', required=True,
                           help="Choose the desired saving format (pdf, png, svg")
-    cli_args.add_argument('--timelabels', type=str, action='store', required=False, default=[], nargs='+',
-                          help="Enter the timelabels, e.g.: 2018 2019 2020")
+    cli_args.add_argument('--timelabels', type=str, action='store', required=False, default=[""], nargs='+',
+                          help="Optional: Enter the timelabels, e.g.: 2018 2019 2020")
     cli_args.add_argument('--verbosity', type=int, action='store', required=False, default=3,
-                          help='Choose the verbosity level (1 = low, 4 = high') 
-    cli_args.add_argument('--clearchache', type=bool, action='store_true', required=False, default=False,
-                          help='Choose "True" or "False" to clear the cache. Necessary when plot sizing is changend.')               
+                          help='Optional: Choose the verbosity level (1 = low, 4 = high') 
+    cli_args.add_argument('--clearchache', action='store_true', default=False,
+                         help='Optinal: Choose "True" or "False" to clear the cache. Necessary when plot sizing is changend.')               
     ARGS = cli_args.parse_args()
 
     #Codebook einlsen
@@ -36,7 +40,7 @@ def main():
     with open(ARGS.pathtoconfigfile, "r") as f:
         config = yaml.full_load(f)
 
-    with open("niceconfig.yml", "r") as f:
+    with open(f"{os.path.dirname(__file__)}/niceconfig.yml", "r") as f:
         niceconfig = yaml.full_load(f)
     
     #Filter in niceconfig überschreiben mit den in XXX'example'XXX definierten fildern
@@ -52,17 +56,20 @@ def main():
 
     #overrides the config-defaults of 'niceplots' ('niceconfig') with the settings in XXX'example'XXX
     for key, value in config.items():
-        position = np.where(codebook["Variable Name"] == key)[0][0]
-    for innerkey, innervalue in value.items():
-        if innerkey != "Group":
-            codebook.at[position, innerkey + " - nice-plots"] = innervalue
-        else:
-            codebook.at[position, innerkey] = innervalue
+        if key == "filters":
+            continue
+        position = np.where(codebook[niceconfig["name_label"]] == key)[0][0]
+        for innerkey, innervalue in value.items():
+            if innerkey != "Group":
+                codebook.at[position, innerkey + " - nice-plots"] = innervalue
+            else:
+                codebook.at[position, innerkey] = innervalue
+ 
     
     #last main here is the function "main()"
     from niceplots.main import main
 
-    main(ARGS.plottype, [ARGS.pathtodata], "niceconfig.yml", codebook, ARGS.outputname, ARGS.clearchache, ARGS.saveas, ARGS.timelabels, ARGS.verbosity)
+    main(ARGS.plottype, [ARGS.pathtodata], niceconfig, codebook, ARGS.outputname, ARGS.clearchache, ARGS.saveas, ARGS.timelabels, ARGS.verbosity, ARGS.saveto)
 
 if __name__ == "__main__":
     main()
